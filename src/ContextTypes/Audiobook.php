b3e0ba908f711dba04ed5baf5567bdfeb3e73aa4
Abraham Arango <Abraham.ArangoC@gmail.com>
2019-04-14 17:58:01 -0400

    Book and Media objects
    Adding Audio Book, Book, Book Format Type, Enumeration, and Media Object to list of schema objects.

---
+++ src/ContextTypes/Audiobook.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JsonLd\ContextTypes;
+
+/**
+ * The publication format of the book.
+ * https://schema.org/BookFormatType
+ */
+class Audiobook extends Book
+{
+    /**
+     * Property structure
+     *
+     * @var array
+     */
+    protected $extendedStructure = [
+        'caption'       => MediaObject::class,
+        'transcript'    => null,
+        'duration'      => Duration::class,
+        'readBy'        => Person::class,
+    ];
+
+    /**
+     * Constructor. Merges extendedStructure up
+     *
+     * @param array $attributes
+     * @param array $extendedStructure
+     */
+    public function __construct(array $attributes, array $extendedStructure = [])
+    {
+        parent::__construct($attributes, array_merge($this->structure, $this->extendedStructure, $extendedStructure));
+    }
+}
